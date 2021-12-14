15b4709c27443650597bed2f0a60cc7f45dfccf8
tars003 <sharmaajay2kuu@gmail.com>
2021-12-14 21:48:10 +0530

    14 dec push

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "31e0258c3e4126f5f6c0a670b2677e28",
+  "1811%201302.zip": "1723439c03c96a4d6306dd8eeafab306",
+"assets/AssetManifest.json": "31e0258c3e4126f5f6c0a670b2677e28",
 "assets/assets/fonts/OpenSans-Bold.ttf": "1025a6e0fb0fa86f17f57cc82a6b9756",
 "assets/assets/fonts/OpenSans-Italic.ttf": "f6238deb7f40a7a03134c11fb63ad387",
 "assets/assets/fonts/OpenSans-Regular.ttf": "3ed9575dcc488c3e3a5bd66620bdf5a4",
@@ -21,9 +22,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "b18446be6a002d11189754f4ce2e59a9",
-"/": "b18446be6a002d11189754f4ce2e59a9",
-"main.dart.js": "243581253dededabd08b2c4003838957",
+"index.html": "5a5b8af2f73935435b359ed5f44b44f6",
+"/": "5a5b8af2f73935435b359ed5f44b44f6",
+"main.dart.js": "54523618ec1f8a306a270ba5e1ba241d",
 "manifest.json": "bc00932895eeaee8bbc6cf927a3d5b78",
 "version.json": "5c855a4892508028794a480999adcc51"
 };
